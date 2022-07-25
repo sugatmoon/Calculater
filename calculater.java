bed2750aed4fecd7898857c45472c32f72f99c49
sugatmoon <97450000+sugatmoon@users.noreply.github.com>
2022-07-25 15:07:35 +0530

    Update and rename secondfile.java to calculater.java

--- calculater.java
+++ calculater.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class secondfile {
+public class calculater {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.print("  ");
